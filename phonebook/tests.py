3a818384eb3d12fedb4be39006c2ff0eb4396a7b
klebanoff <klebanov1999@yandex.ru>
2021-10-27 16:25:26 +0300

    2

--- phonebook/tests.py
+++ phonebook/tests.py
@@ -69,3 +69,73 @@ class NewDepView(TestCase):
         response = self.client.get(reverse('phonebook:departments'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Отдел")
+class NewEmployeeView(TestCase):
+    def test_newempl(self):
+        db = Department()
+        db.new('Отдел',0)
+        id = 1
+        client = Client()
+        client.post(reverse('phonebook:newemplyee', args=(id,)),
+                    {'name': 'Имя',
+                     'surname': 'Фамилия',
+                     'partonymic': 'Отчество',
+                     'email': 'Email',
+                     'phonenumber': 'номер телефона'})
+        response = self.client.get(reverse('phonebook:search'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Имя")             
+class DeleteEmployee(TestCase):
+    def test_deleteempoyee(self):
+        db = Department()
+        db.new('Отдел',0)
+        id = 1
+        client = Client()
+        client.post(reverse('phonebook:newemplyee', args=(id,)),
+                    {'name': 'Имя',
+                     'surname': 'Фамилия',
+                     'partonymic': 'Отчество',
+                     'email': 'Email',
+                     'phonenumber': 'номер телефона'})
+        response = self.client.get(reverse('phonebook:deleteemployee', args=(1,)))
+        response = self.client.get(reverse('phonebook:search'))
+        self.assertQuerysetEqual(response.context['employees'], [])
+class EmployeeView(TestCase):
+    def test_employeeview(self):
+        db = Department()
+        db.new('Отдел',0)
+        id = 1
+        client = Client()
+        client.post(reverse('phonebook:newemplyee', args=(id,)),
+                    {'name': 'Имя',
+                     'surname': 'Фамилия',
+                     'partonymic': 'Отчество',
+                     'email': 'Email',
+                     'phonenumber': 'номер телефона'})
+        response = self.client.get(reverse('phonebook:employee', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Имя")          
+class Search(TestCase):
+    def test_search(self):
+        db = Department()
+        db.new('Отдел',0)
+        id = 1
+        client = Client()
+        client.post(reverse('phonebook:newemplyee', args=(id,)),
+                    {'name': 'Имя',
+                     'surname': 'Фамилия',
+                     'partonymic': 'Отчество',
+                     'email': 'Email',
+                     'phonenumber': 'номер телефона'})
+        client.post(reverse('phonebook:newemplyee', args=(id,)),
+                    {'name': '2Имя',
+                     'surname': '2Фамилия',
+                     'partonymic': '2Отчество',
+                     'email': '2Email',
+                     'phonenumber': '2номер телефона'})
+        response = client.get('/phonebook/employee/all?q=Имя')
+        employees = Employee()
+        employee = employees.getbyid(1)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Имя")              
+        self.assertQuerysetEqual(response.context['employees'], employee)           
+                   
